0bb700ad1d416a96a8875c3f956fc8ce32d3c26d
Konctantin <gawrilyako@gmail.com>
2013-07-25 11:34:53 +0400

    Move process list to new classe
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

---
+++ Yanitta/Misk/ProcessList.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace Yanitta
+{
+    public class ProcessList : ObservableCollection<WowMemory>, IDisposable
+    {
+        private DispatcherTimer refreshTimer;
+
+        public ProcessList()
+            : base()
+        {
+            this.refreshTimer = new DispatcherTimer();
+            this.refreshTimer.Interval = TimeSpan.FromSeconds(1);
+            this.refreshTimer.Tick += (o, e) => CheckProcess();
+#if !TRACE
+            this.refreshTimer.IsEnabled = true;
+            this.refreshTimer.Start();
+#else
+            new Thread(new ThreadStart(() => {
+                Thread.Sleep(5000);
+                foreach (WowClass wclass in Enum.GetValues(typeof(WowClass)))
+                {
+                    App.Current.Dispatcher.BeginInvoke(new Action(() => {
+                        this.Add(new WowMemory(wclass, wclass.ToString(), 0, ((byte)wclass == 1)));
+                    }));                    
+                }
+            })).Start();
+#endif
+        }
+
+        private void CheckProcess()
+        {
+            var wowProcessList = Process.GetProcessesByName("wow");
+
+            if (!wowProcessList.Any())
+            {
+                foreach (var process in this)
+                {
+                    Console.WriteLine("Dispose dead process [" + process.ProcessId + "]");
+                    process.Dispose();
+                }
+                this.Clear();
+                return;
+            }
+
+            for (int i = this.Count - 1; i >= 0; --i)
+            {
+                if (!wowProcessList.Any(n => n.Id == this[i].ProcessId))
+                {
+                    Console.WriteLine("Dispose dead process [" + this[i].ProcessId + "]");
+                    this[i].Dispose();
+                    this.RemoveAt(i);
+                }
+            }
+
+            foreach (var wowProcess in wowProcessList)
+            {
+                if (this.Any(n => n.ProcessId == wowProcess.Id))
+                    continue;
+
+                try
+                {
+                    var wowMemory = new WowMemory(wowProcess);
+
+                    if (!wowMemory.IsInGame)
+                    {
+                        wowMemory.Dispose();
+                        continue;
+                    }
+
+                    wowMemory.GameStateChanged += (memory) => {
+                        if (!memory.IsInGame)
+                        {
+                            if (this.Contains(memory))
+                                this.Remove(memory);
+                            memory.Dispose();
+                        }
+                    };
+                    this.Add(wowMemory);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error WowMemory: {0}", ex.Message);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            if (this.refreshTimer != null)
+            {
+                this.refreshTimer.IsEnabled = false;
+                this.refreshTimer.Stop();
+                this.refreshTimer = null;
+            }
+
+            foreach (var process in this)
+                process.Dispose();
+            this.Clear();
+        }
+    }
+}
--- Yanitta/Windows/MainWindow.xaml.cs
+++ Yanitta/Windows/MainWindow.xaml.cs
@@ -17,108 +17,27 @@ namespace Yanitta
     /// </summary>
     public partial class MainWindow : Window
     {
-        private DispatcherTimer refreshTimer = null;
-        private WinCodeExecute codeExecuteWindow = null;
+        private WinCodeExecute   codeExecuteWindow = null;
         private WinProfileEditor profileWindows = null;
-        private WindowSettings settingWindow = null;
+        private WindowSettings   settingWindow = null;
 
-        public static ObservableCollection<WowMemory> ProcessList { get; set; }
-
+        public static ProcessList ProcessList { get; set; }
         public static ObservableCollection<IYanittaPlugin> PluginList { get; set; }
 
         static MainWindow()
         {
-            ProcessList = new ObservableCollection<WowMemory>();
-            PluginList = (App.Current as App).PluginList;
+            ProcessList = new ProcessList();
+            PluginList  = (App.Current as App).PluginList;
         }
 
         public MainWindow()
         {
             InitializeComponent();
 
-            ProcessList.CollectionChanged += (o, e) =>
-            {
+            ProcessList.CollectionChanged += (o, e) => {
                 if (ProcessList.Count > 0 && lbProcessList.SelectedIndex == -1)
                     lbProcessList.SelectedIndex = 0;
             };
-
-            refreshTimer = new DispatcherTimer();
-            refreshTimer.Interval = TimeSpan.FromSeconds(1);
-            refreshTimer.Tick += (o, e) => UpdateProcessList();
-#if !TRACE
-            refreshTimer.IsEnabled = true;
-            refreshTimer.Start();
-#else
-            int i = 0;
-            foreach (WowClass wclass in Enum.GetValues(typeof(WowClass)))
-            {
-                ProcessList.Add(new WowMemory(wclass, wclass.GetLocalizedName(), 0, (i++ == 0)));
-            }
-#endif
-        }
-
-        private void UpdateProcessList()
-        {
-            var wowProcessList = Process.GetProcessesByName("wow");
-
-            #region Чистка "мертвых процессов"
-
-            // Если запущенных процессов нет, тогда просто очищаем список
-            if (!wowProcessList.Any())
-            {
-                foreach (var process in ProcessList)
-                {
-                    Console.WriteLine("Dispose dead process [" + process.ProcessId + "]");
-                    process.Dispose();
-                }
-                ProcessList.Clear();
-                return;
-            }
-
-            // далее, проверяем есть ли в списке "мертвые процессы"
-            for (int i = ProcessList.Count - 1; i >= 0; --i)
-            {
-                if (!wowProcessList.Any(n => n.Id == ProcessList[i].ProcessId))
-                {
-                    Console.WriteLine("Dispose dead process [" + ProcessList[i].ProcessId + "]");
-                    ProcessList[i].Dispose();
-                    ProcessList.RemoveAt(i);
-                }
-            }
-
-            #endregion Чистка "мертвых процессов"
-
-            foreach (var wowProcess in wowProcessList)
-            {
-                if (ProcessList.Any(n => n.ProcessId == wowProcess.Id))
-                    continue;
-
-                try
-                {
-                    var wowMemory = new WowMemory(wowProcess);
-
-                    if (!wowMemory.IsInGame)
-                    {
-                        wowMemory.Dispose();
-                        continue;
-                    }
-
-                    wowMemory.GameStateChanged += (memory) =>
-                    {
-                        if (!memory.IsInGame)
-                        {
-                            if (ProcessList.Contains(memory))
-                                ProcessList.Remove(memory);
-                            memory.Dispose();
-                        }
-                    };
-                    ProcessList.Add(wowMemory);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Error WowMemory: {0}", ex.Message);
-                }
-            }
         }
 
         private void Image_MouseDown(object o, RoutedEventArgs e)
@@ -128,23 +47,12 @@ namespace Yanitta
 
         private void Window_Closing(object sender, CancelEventArgs e)
         {
-            if (refreshTimer != null)
-            {
-                refreshTimer.IsEnabled = false;
-                refreshTimer.Stop();
-                refreshTimer = null;
-            }
-
             foreach (var plugin in PluginList)
                 plugin.Dispose();
             PluginList.Clear();
 
             if (ProcessList != null)
-            {
-                foreach (var wow in ProcessList)
-                    wow.Dispose();
-                ProcessList.Clear();
-            }
+                ProcessList.Dispose();
 
             if (codeExecuteWindow != null)
             {
