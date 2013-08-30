34f16a0c1a10bed1fb42c6e05e38f96eaa337f32
Konctantin <gawrilyako@gmail.com>
2013-08-30 17:55:38 +0400

    clenuaps Offsets class
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- Yanitta/XML/Offsets.cs
+++ Yanitta/XML/Offsets.cs
@@ -1,116 +1,69 @@
 ﻿using System;
-using System.ComponentModel;
 using System.IO;
+using System.Windows;
 using System.Xml.Serialization;
 
 namespace Yanitta
 {
     [Serializable]
-    public class Offsets : INotifyPropertyChanged
+    public class Offsets : DependencyObject
     {
         [XmlIgnore]
+        public static readonly DependencyProperty BuildProperty                         = DependencyProperty.Register("Build",                          typeof(int),  typeof(Offsets));
+        [XmlIgnore]
+        public static readonly DependencyProperty PlayerNameProperty                    = DependencyProperty.Register("PlayerName",                     typeof(long), typeof(Offsets));
+        [XmlIgnore]
+        public static readonly DependencyProperty PlayerClassProperty                   = DependencyProperty.Register("PlayerClass",                    typeof(long), typeof(Offsets));
+        [XmlIgnore]
+        public static readonly DependencyProperty IsInGameProperty                      = DependencyProperty.Register("IsInGame",                       typeof(long), typeof(Offsets));
+        [XmlIgnore]
+        public static readonly DependencyProperty FrameScript_ExecuteBufferProperty     = DependencyProperty.Register("FrameScript_ExecuteBuffer",      typeof(long), typeof(Offsets));
+        [XmlIgnore]
+        public static readonly DependencyProperty FrameScript_GetLocalizedTextProperty  = DependencyProperty.Register("FrameScript_GetLocalizedText",   typeof(long), typeof(Offsets));
+
+        [XmlIgnore]
         private const string fileName = "offsets.xml";
-
-        [XmlIgnore]
-        private int build;
-
-        [XmlIgnore]
-        private long playerName;
-
-        [XmlIgnore]
-        private long playerClass;
-
-        [XmlIgnore]
-        private long isInGame;
-
-        [XmlIgnore]
-        private long frameScript_ExecuteBuffer;
-
-        [XmlIgnore]
-        private long frameScript_GetLocalizedText;
 
         [XmlElement]
         public int Build
         {
-            get { return this.build; }
-            set
-            {
-                if (value != build)
-                {
-                    build = value;
-                    OnPropertyChanged("Build");
-                }
-            }
+            get { return (int)GetValue(BuildProperty); }
+            set { SetValue(BuildProperty, value); }
         }
 
         [XmlElement]
         public long PlayerName
         {
-            get { return this.playerName; }
-            set
-            {
-                if (value != playerName)
-                {
-                    playerName = value;
-                    OnPropertyChanged("PlayerName");
-                }
-            }
+            get { return (long)GetValue(PlayerNameProperty); }
+            set { SetValue(PlayerNameProperty, value); }
         }
 
         [XmlElement]
         public long PlayerClass
         {
-            get { return this.playerClass; }
-            set
-            {
-                if (value != playerClass)
-                {
-                    playerClass = value;
-                    OnPropertyChanged("PlayerClass");
-                }
-            }
+            get { return (long)GetValue(PlayerClassProperty); }
+            set { SetValue(PlayerClassProperty, value); }
         }
 
         [XmlElement]
         public long IsInGame
         {
-            get { return this.isInGame; }
-            set
-            {
-                if (value != isInGame)
-                {
-                    isInGame = value;
-                    OnPropertyChanged("IsInGame");
-                }
-            }
+            get { return (long)GetValue(IsInGameProperty); }
+            set { SetValue(IsInGameProperty, value); }
         }
 
         [XmlElement]
         public long FrameScript_ExecuteBuffer
         {
-            get { return this.frameScript_ExecuteBuffer; }
-            set
-            {
-                if (value != frameScript_ExecuteBuffer)
-                {
-                    frameScript_ExecuteBuffer = value;
-                    OnPropertyChanged("FrameScript_ExecuteBuffer");
-                }
-            }
+            get { return (long)GetValue(FrameScript_ExecuteBufferProperty); }
+            set { SetValue(FrameScript_ExecuteBufferProperty, value); }
         }
 
         [XmlElement]
         public long FrameScript_GetLocalizedText
         {
-            get { return this.frameScript_GetLocalizedText; }
-            set
-            {
-                if (value != frameScript_GetLocalizedText)
-                {
-                    frameScript_GetLocalizedText = value;
-                    OnPropertyChanged("FrameScript_GetLocalizedText");
-                }
-            }
+            get { return (long)GetValue(FrameScript_GetLocalizedTextProperty); }
+            set { SetValue(FrameScript_GetLocalizedTextProperty, value); }
         }
 
         static Offsets()
@@ -126,16 +79,7 @@ namespace Yanitta
 
         public void Save()
         {
-            new XmlManager(fileName)
-                .Save<Offsets>(this);
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged(string propName)
-        {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            new XmlManager(fileName).Save<Offsets>(this);
         }
     }
 }
