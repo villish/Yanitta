1e10a46f9fe5560f3f38d2afbd82d95b81d5baae
Konctantin <gawrilyako@gmail.com>
2014-06-02 09:29:57 +0400

    Удален ненужный конвертер
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- Yanitta/Misk/Converters/YanittaConverters.cs
+++ Yanitta/Misk/Converters/YanittaConverters.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,14 +7,6 @@ using System.Windows.Media.Imaging;
 
 namespace Yanitta
 {
-    public class CollectionToCountConverter : ConverterBase<CollectionToCountConverter>
-    {
-        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return (value is IList) ? (value as IList).Count : 0;
-        }
-    }
-
     public class EnumToImageConverter : ConverterBase<EnumToImageConverter>
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
