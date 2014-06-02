3e3b30c9575ceae785c311e2738ee3ba2fe84dfb
Konctantin <gawrilyako@gmail.com>
2014-06-02 09:54:51 +0400

    Fixed designtime no critical errors.
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- Yanitta/Misk/Converters/ConverterBase.cs
+++ Yanitta/Misk/Converters/ConverterBase.cs
@@ -8,6 +8,11 @@ namespace Yanitta
     public abstract class ConverterBase<T>
         : MarkupExtension, IValueConverter where T : class, new()
     {
+        public ConverterBase()
+            : base()
+        {
+        }
+
         public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
 
         public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
