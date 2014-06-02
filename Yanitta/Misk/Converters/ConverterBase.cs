68b59b8ee91ac18054ed8bee031c77a40f3216af
Konctantin <gawrilyako@gmail.com>
2014-06-02 09:23:50 +0400

    Изменена система конвертеров:  * Вместо ресурсов используется расширение разметки
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

---
+++ Yanitta/Misk/Converters/ConverterBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Yanitta
+{
+    public abstract class ConverterBase<T>
+        : MarkupExtension, IValueConverter where T : class, new()
+    {
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (m_converter == null)
+                m_converter = new T();
+            return m_converter;
+        }
+
+        private static T m_converter = null;
+    }
+}
---
+++ Yanitta/Misk/Converters/YanittaConverters.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Yanitta
+{
+    public class CollectionToCountConverter : ConverterBase<CollectionToCountConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is IList) ? (value as IList).Count : 0;
+        }
+    }
+
+    public class EnumToImageConverter : ConverterBase<EnumToImageConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is WowClass))
+                return Binding.DoNothing;
+
+            var name = (WowClass)value == (WowClass)(byte)0 ? "None" : value.ToString();
+            var path = string.Format(@"pack://application:,,,/Yanitta;component/Resources/{0}.png", name);
+            return new BitmapImage(new Uri(path));
+        }
+    }
+
+    public class EnumToLocalizedSrtConverter : ConverterBase<EnumToLocalizedSrtConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || !(value is Enum))
+                return Binding.DoNothing;
+
+            var name = string.Format("{0}_{1}", value.GetType().Name, value);
+            if (parameter is string && !string.IsNullOrWhiteSpace((string)parameter))
+                name += "_" + parameter;
+            return Localization.ResourceManager.GetString(name, CultureInfo.CurrentUICulture);
+        }
+    }
+
+    public class ListItemIndexConverter : ConverterBase<ListItemIndexConverter>
+    {
+        // {Binding RelativeSource={RelativeSource FindAncestor, AncestorType=ListViewItem}, Converter={yanitta:ListItemIndexConverter}}
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = value as DependencyObject;
+            var view = ItemsControl.ItemsControlFromItemContainer(item);
+            var index = view.ItemContainerGenerator.IndexFromContainer(item);
+            return index + 1;
+        }
+    }
+
+    public class ResizeConverter : ConverterBase<ResizeConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double mod = 0d;
+            double val = 0d;
+            if (value == null)
+                return 0d;
+            if (value is double)
+                val = (double)value;
+            if (double.IsNaN(val))
+                val = 100d;
+            if (parameter is string)
+                double.TryParse((string)parameter, out mod);
+            return val + mod;
+        }
+    }
+
+    public class StringToIntConverter : ConverterBase<StringToIntConverter>
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.Format("0x{0:X}", value);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return Binding.DoNothing;
+
+            string str = value.ToString();
+
+            if (string.IsNullOrWhiteSpace(str))
+                return Binding.DoNothing;
+
+            long n;
+            if (IsHex(str))
+            {
+                if (str.StartsWith("0x"))
+                    long.TryParse(str.Substring(2), NumberStyles.AllowHexSpecifier, CultureInfo.CurrentCulture, out n);
+                else
+                    long.TryParse(str, NumberStyles.AllowHexSpecifier, CultureInfo.CurrentCulture, out n);
+            }
+            else
+                long.TryParse(str, out n);
+
+            return n;
+        }
+
+        private bool IsHex(string str)
+        {
+            if (!string.IsNullOrWhiteSpace(str))
+                foreach (char c in str.ToUpper())
+                {
+                    if (c >= 'A' && c <= 'F')
+                        return true;
+                }
+            return false;
+        }
+    }
+}
