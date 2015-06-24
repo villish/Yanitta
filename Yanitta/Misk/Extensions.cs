4c15ac7e496be1d5196568fad4c271fdd5a1538c
Konctantin <gawrilyako@gmail.com>
2015-06-24 10:05:27 +0300

    drop unused extension method
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- Yanitta/Misk/Extensions.cs
+++ Yanitta/Misk/Extensions.cs
@@ -17,15 +17,6 @@ namespace Yanitta
             return control == null ? null : control.SelectedValue as T;
         }
 
-        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> predicate)
-        {
-            if (collection == null)
-                return;
-
-            foreach (T element in collection)
-                predicate(element);
-        }
-
         public static void AppendFormatLine(this StringBuilder builder, string format, params object[] args)
         {
             if (args == null || args.Length == 0)
