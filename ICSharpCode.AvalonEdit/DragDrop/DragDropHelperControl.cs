927b5271da3df0ab4d6dd00743ac3399b39a3f95
Konctantin <gawrilyako@gmail.com>
2015-06-29 11:31:39 +0300

    Implement DragDrop items in LiestView control
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

---
+++ ICSharpCode.AvalonEdit/DragDrop/DragDropHelperControl.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ICSharpCode.AvalonEdit
+{
+    public class DragDropHelperControl : Control
+    {
+        static DragDropHelperControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DragDropHelperControl), new FrameworkPropertyMetadata(typeof(DragDropHelperControl)));
+        }
+    }
+}
