9c9c9f96df424539938c07292997fbece9cc8849
Konctantin <gawrilyako@gmail.com>
2016-02-10 11:29:58 +0300

    upd
    Signed-off-by: Konctantin <gawrilyako@gmail.com>

--- compile.bat
+++ compile.bat
@@ -1,5 +1,9 @@
+git pull origin master
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+
 "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" /m Yanitta.sln /p:Configuration=Release "/p:Platform=Any CPU"
 @IF %ERRORLEVEL% NEQ 0 GOTO err
+
 @exit /B 0
 :err
 @PAUSE
